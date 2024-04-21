d2d5a274f67cedc37e581202664949ff4d43a366
James Kinyua <jameskinyua590@gmail.com>
2024-04-21 13:44:57 +0300

    remove defaultViewPort
    Signed-off-by: James Kinyua <jameskinyua590@gmail.com>

--- task1/src/config.js
+++ task1/src/config.js
@@ -15,10 +15,7 @@ const config = {
   BUILDING_LOCATION: '경기도 고양시 일산동구 강석로 152 강촌마을아파트 제701동 제2층 제202호 [마두동 796]',
   BROWSER_OPTIONS: { 
     headless: false,
-    defaultViewport: {
-      width: 1920,
-      height: 1080,
-    },
+    defaultViewport: null,
     args: ['--start-fullscreen']
   }
 };
